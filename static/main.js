1bb1cef6260eadbff8b1acf4c083483dba46fc2b
cywwee <kyrylo1buryk.it@gmail.com>
2025-03-07 00:15:46 +0100

    refactor: all old code and made new website visual

--- static/main.js
+++ static/main.js
@@ -1,15 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".event").forEach(event => {
-        event.addEventListener("click", () => {
-            let dropdown = event.querySelector(".dropdown");
-            dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-        });
-    });
-
-    document.querySelectorAll(".card").forEach(card => {
-        card.addEventListener("click", () => {
-            let content = card.querySelector(".card-content");
-            content.style.display = content.style.display === "block" ? "none" : "block";
-        });
+    AOS.init({
+        duration: 1000, // Длительность анимации (1 секунда)
+        once: true      // Анимация проигрывается только один раз
     });
 });
